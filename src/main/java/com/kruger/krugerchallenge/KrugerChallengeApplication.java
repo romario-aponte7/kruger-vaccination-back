5f67983bb8ce061ba41e39ea24887ef1131684dd
romario <romario151996@gmail.com>
2022-12-02 10:18:57 -0500

    init

---
+++ src/main/java/com/kruger/krugerchallenge/KrugerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.kruger.krugerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrugerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrugerChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kruger/krugerchallenge/KrugerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kruger.krugerchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrugerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
